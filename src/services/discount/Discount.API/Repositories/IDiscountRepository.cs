e30cb5b7fc7d39bf461d74744984172a538b2876
Robert Rancz <rancz.robi@gmail.com>
2021-07-08 21:56:02 +0300

    Create Discount microservice.

---
+++ src/services/discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscountAsync(string productName);
+        Task<bool> CreateDiscountAsync(Coupon coupon);
+        Task<bool> UpdateDiscountAsync(Coupon coupon);
+        Task<bool> DeleteDiscountAsync(string productName);
+    }
+}
