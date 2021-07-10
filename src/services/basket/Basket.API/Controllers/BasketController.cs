bd679a6984c8ee4722466accfc421657c70381a6
Robert Rancz <rancz.robi@gmail.com>
2021-07-10 22:57:26 +0300

    Consume the discount gRPC service using a gRPC client

--- src/services/basket/Basket.API/Controllers/BasketController.cs
+++ src/services/basket/Basket.API/Controllers/BasketController.cs
@@ -1,6 +1,8 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -11,10 +13,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
-            _repository = repository;
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -29,6 +33,14 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            // Communicate with Discount gRPC service to calculate discounted prices - if applicable - for each cart item
+            // This kind of inter-service communication is performance critical, hence using the gRPC model.
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscountAsync(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+
             return Ok(await _repository.UpdateBasketAsync(basket));
         }
 
