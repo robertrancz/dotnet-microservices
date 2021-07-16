29f2af2585a2927b9129594237804110f87b29aa
Robert Rancz <rancz.robi@gmail.com>
2021-07-16 21:23:35 +0300

    Implement Infrastructure layer: repositories and email service with SendGrid

---
+++ src/services/ordering/Ordering.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Email
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmailAsync(Application.Models.Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var body = email.Body;
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation($"Email sent successfully");
+                return true;
+            }
+            else
+            {
+                _logger.LogError($"Email could not be sent. Response status code: {response.StatusCode}");
+                return false;
+            }
+        }
+    }
+}
