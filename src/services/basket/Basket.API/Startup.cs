0b427052c8d0a6c177177de2b61c8dc4689f1ac9
Robert Rancz <rancz.robi@gmail.com>
2021-07-18 10:01:04 +0300

    Set up the async communication with RabbitMq using MassTransit

--- src/services/basket/Basket.API/Startup.cs
+++ src/services/basket/Basket.API/Startup.cs
@@ -1,18 +1,14 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.API
 {
@@ -40,6 +36,15 @@ namespace Basket.API
                 o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]);
             });
             services.AddScoped<DiscountGrpcService>();
+
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            services.AddMassTransitHostedService();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
